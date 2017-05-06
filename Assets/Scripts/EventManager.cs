16549025d10f12a76d8fc1510bad085cd54a80ca
Michael Demone <m_d_emone@hotmail.com>
2017-05-05 23:57:38 -0700

    Added initial event system and a Stat.cs example class.

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+
+    public static EventManager Instance;
+
+    public static Action SnowballThrown;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FireSnowballThrown()
+    {
+        if (SnowballThrown != null) SnowballThrown();
+    }
+}
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour {
+
+
+    public static int numberOfSnowballsThrown = 0;
+
+	// Use this for initialization
+	void Start () {
+        EventManager.SnowballThrown += RecordSnowballThrown;
+	}
+	
+    void RecordSnowballThrown()
+    {
+        numberOfSnowballsThrown++;
+        print("SNow ball thrown, total number thrown: " + numberOfSnowballsThrown);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
