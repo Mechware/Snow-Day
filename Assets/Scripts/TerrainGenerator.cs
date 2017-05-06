fdb51f3f3901a66d72e551792a0d2a3114cb08c8
Michael Demone <m_d_emone@hotmail.com>
2017-05-06 00:00:07 -0700

    Added terrain generator class

---
+++ Assets/Scripts/TerrainGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+
+
+    public Terrain terrain;
+
+	// Use this for initialization
+	void Start () {
+        TerrainData terrainData = new TerrainData();
+        float[,] heightData = new float[30,30];
+        for(int i = 1; i < heightData.GetLength(0); i++)
+        {
+            for(int j = 0; j < heightData.GetLength(1); j++)
+            {
+                heightData[i, j] = 1f/(float)i;
+                print(string.Format("Height data[{0}, {1}] = {2}", i, j, heightData[i, j]));
+            }
+        }
+
+        terrainData.SetHeights(0, 0, heightData);
+        terrain.GetComponent<TerrainCollider>().terrainData = terrainData;
+        terrain.terrainData = terrainData;
+        terrain.Flush();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
